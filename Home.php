1853a5f742809795f56de072ad820f24c286a2c3
Fakhri T <33167923+fakhritaufiq@users.noreply.github.com>
2019-09-15 16:29:53 +0700

    Create Home.php

---
+++ Home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <link rel="icon" href="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/846a9086-8a40-43e0-aa10-2fc7d6d73730/dd4byb2-1fc5e70f-4099-4e15-b194-7f450ac735fe.png/v1/fill/w_800,h_800,strp/avengers__endgame__2019__avengers_snap_logo_png__by_mintmovi3_dd4byb2-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODAwIiwicGF0aCI6IlwvZlwvODQ2YTkwODYtOGE0MC00M2UwLWFhMTAtMmZjN2Q2ZDczNzMwXC9kZDRieWIyLTFmYzVlNzBmLTQwOTktNGUxNS1iMTk0LTdmNDUwYWM3MzVmZS5wbmciLCJ3aWR0aCI6Ijw9ODAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.soebOX2_vOWgPcZuFxyAq5rPW2okbZogwJUq0YwF3PE">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <title>Apon</title>
+    <style>
+        .Input_obat {
+            width: 3000px;
+            height: 1000px;
+        }
+    </style>
+</head>
+
+<body>
+    <center>
+        <h1 class="Hello">Hello Admin!</h1>
+        <hr size="30px" width="50%">
+    </center>
+    <div class="row">
+        <div class="Input_obat col-md-4">
+            <center><button type="button" class="btn btn-outline-warning">Input Obat</button></center>
+        </div>
+        <div class="Delete_obat col-md-4">
+            <center><button type="button" class="btn btn-outline-warning">Delete Obat</button></center>
+        </div>
+        <div class="Update_obat col-md-4">
+            <center><button type="button" class="btn btn-outline-warning">Update Obat</button></center>
+        </div>
+    </div>
+</body>
